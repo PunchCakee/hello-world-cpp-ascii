9470bd0aec0bb439e3b988c94b38ff84901a2b24
PunchCakee <email@example.com>
2022-09-12 03:35:00 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    char H = 72;
+    char e = 101;
+    char l = 108;
+    char o = 111;
+
+    char w = 119;
+    char r = 114;
+    char d = 100;
+
+    
+
+    std::cout << H;
+    std::cout << e;
+    std::cout << l;
+    std::cout << l;
+    std::cout << o;
+
+    std::cout << w;
+    std::cout << o;
+    std::cout << r;
+    std::cout << l;
+    std::cout << d;
+    std::cin.get();
+    return 0;
+}
